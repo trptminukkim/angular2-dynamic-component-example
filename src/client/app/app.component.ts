116c092a569152a0d501c82564a7aafc72ebad82
KimSource <minukkim@gimmin-ug-ui-Mac-mini.local>
2016-08-29 15:55:47 +0900

    fix
    fix

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 
 import { BlockComponent } from './block.component';
